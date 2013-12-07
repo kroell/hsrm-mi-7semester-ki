3ef140a84496b751292545b3a42b3a8d350550ff
Soeren Kroell <kroell@idseven.de>
2013-12-07 01:16:17 +0100

    Description wird aus den Meta-Tags ausgelesen

--- SearchEngine/model/Spider.py
+++ SearchEngine/model/Spider.py
@@ -49,20 +49,22 @@ class Spider(object):
             description = soup.find("meta", {"name":"description"})
             if (description):
                 description = description['content']
-        print description
         created = modified = time.time()
         page = Page(html, docurl, title, description, created, modified)
         self.pages.append(page)
         
         for link in soup.find_all('a'):
             url = link.get('href')
-            if url.startswith('/') or url.startswith(self.start_url):
-                if url.startswith('/'):
-                    url = self.start_url + url[1:]
-                if url not in [page.url for page in self.pages]:
-                    print url
-                    #time.sleep(5) 
-                    self.initSpider(url)
+            try:
+                if url.startswith('/') or url.startswith(self.start_url):
+                    if url.startswith('/'):
+                        url = self.start_url + url[1:]
+                    if url not in [page.url for page in self.pages]:
+                        print url
+                        #time.sleep(5) 
+                        self.initSpider(url)
+            except Exception, error:
+                print "Error: %r" % error
         
     
     def preparePage(self):
